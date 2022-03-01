645b0e7f8b512d04e97433311af8f527ba904a7f
kevin-rodri <kr399880@gmail.com>
2022-02-28 21:10:29 -0500

    changed the range of the random number to 200

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,14 +1,14 @@
 
 public class DmvSimulator {
 	public static void main(String[] args) {
-		int waitTime = (int)(Math.random()*100);
+		int waitTime = (int)(Math.random()*200);
 		boolean keepGoing = true;
 		boolean keep = true;
 		System.out.println("Welcome to the DMV! You are " + waitTime + " in line. Please wait until it is your turn. ");
-		for (int wait = waitTime + 1; wait < 100; wait ++) {
+		for (int wait = waitTime + 1; wait < 200; wait ++) {
 			System.out.println("Number " + wait + " is next.");
-		
 			}
+		
 		for(int i = 0; i <= waitTime ; i ++) {
 				System.out.println("Number " + i+ " is next.");
 				if(i == waitTime) {
