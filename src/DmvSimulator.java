226503e55dfdec586188828a54c4f6948a8f4669
kevin-rodri <kr399880@gmail.com>
2022-02-28 21:00:08 -0500

    a dmv simulator that welcomes user, generates a number from 1 to 100, iterate, and inform user that they do not have the paperwork

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,9 +1,17 @@
 
 public class DmvSimulator {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int waitTime = (int)(Math.random()*100);
+		boolean keepGoing = true;
+		while (keepGoing) {
+			System.out.println("Welcome to the DMV! You are " + waitTime + " in line. Please wait until it is your turn. ");
+			waitTime -= 1;
+			System.out.println("Number " + waitTime + " is next.");
+			if(waitTime == 1) {
+				System.out.println("It is your turn! It looks like you do not have the required paperwork. See ya later!");	
+				keepGoing = false;
+			}
 
+		}
 	}
-
 }
