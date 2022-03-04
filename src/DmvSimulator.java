9c5fd0aec8b4e209d16a782042903337d0173b65
kevin-rodri <kr399880@gmail.com>
2022-03-04 16:48:12 -0500

    added 99% chance user will likely get told that they do not have the required paperwork

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,20 +1,20 @@
 
 public class DmvSimulator {
 	public static void main(String[] args) {
-		int waitTime = (int)(Math.random()*100);
+		int waitTime = (int) (Math.random() * 200);
 		boolean keepGoing = true;
 		boolean keep = true;
 		System.out.println("Welcome to the DMV! You are " + waitTime + " in line. Please wait until it is your turn. ");
-		for (int wait = waitTime + 1; wait < 100; wait ++) {
+		for (int wait = waitTime + 1; wait < 200; wait++) {
 			System.out.println("Number " + wait + " is next.");
-		
+
+		}
+		for (int i = 0; i <= waitTime; i++) {
+			System.out.println("Number " + i + " is next.");
+			if (i == waitTime) {
+				System.out.println(
+						"It is your turn! It looks like you do not have the required paperwork. So go home and leave us alone!");
 			}
-		for(int i = 0; i <= waitTime ; i ++) {
-				System.out.println("Number " + i+ " is next.");
-				if(i == waitTime) {
-					System.out.println("It is your turn! It looks like you do not have the required paperwork. So go home and leave us alone!");	
-					}
-			 	}
-			}
+		}
 	}
-	
+}
