48c925412e78f3dce626eb970c7281e8ad4b7999
kevin-rodri <kr399880@gmail.com>
2022-03-04 16:40:06 -0500

    idented

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,20 +1,20 @@
 
 public class DmvSimulator {
 	public static void main(String[] args) {
-		int waitTime = (int)(Math.random()*200);
+		int waitTime = (int) (Math.random() * 200);
 		boolean keepGoing = true;
 		boolean keep = true;
 		System.out.println("Welcome to the DMV! You are " + waitTime + " in line. Please wait until it is your turn. ");
-		for (int wait = waitTime + 1; wait < 200; wait ++) {
+		for (int wait = waitTime + 1; wait < 200; wait++) {
 			System.out.println("Number " + wait + " is next.");
+		}
+
+		for (int i = 0; i <= waitTime; i++) {
+			System.out.println("Number " + i + " is next.");
+			if (i == waitTime) {
+				System.out.println(
+						"It is your turn! It looks like you do not have the required paperwork. See you later!");
 			}
-		
-		for(int i = 0; i <= waitTime ; i ++) {
-				System.out.println("Number " + i+ " is next.");
-				if(i == waitTime) {
-					System.out.println("It is your turn! It looks like you do not have the required paperwork. See you later!");	
-					}
-			 	}
-			}
+		}
 	}
-	
+}
