4b523aa766758fe8333a6fc3cccaf940ee543044
kevin-rodri <kr399880@gmail.com>
2022-03-07 12:00:04 -0500

    fixed merge conflict

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -2,6 +2,7 @@
 public class DmvSimulator {
 	public static void main(String[] args) {
 		int waitTime = (int) (Math.random() * 200);
+		int prob = (int) (Math.random() * 100);
 		boolean keepGoing = true;
 		boolean keep = true;
 		System.out.println("Welcome to the DMV! You are " + waitTime + " in line. Please wait until it is your turn. ");
@@ -12,8 +13,12 @@ public class DmvSimulator {
 		for (int i = 0; i <= waitTime; i++) {
 			System.out.println("Number " + i + " is next.");
 			if (i == waitTime) {
+				if (prob <= 99) {
 				System.out.println(
-						"It is your turn! It looks like you do not have the required paperwork. So go home and leave us alone!");
+						"It is your turn! It looks like you do not have the required paperwork. So go home and leave us alone! BWAAAAHHH");
+				} else {
+					System.out.println("You are all set!");
+				}
 			}
 		}
 	}
